43ea28471ba0217054f5fe88ddadfbe8f0566e26
soniahoma <sonia_homa@yahoo.com>
2020-04-03 15:55:55 -0700

    Leighann/Sonia pair program

--- wayfarer-backend/server.js
+++ wayfarer-backend/server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const cors = require('cors');
-const session = require('express-session')
+const cors = require('cors');
+const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 require('dotenv').config()
@@ -17,9 +17,31 @@ const routes = require('./routes');
 // database
 const db = require('./models');
 
+// const corsOptions = {
+    // origin: ['http://localhost:3000'],
+    // // methods: "GET,POST,PUT,DELETE",
+    // credentials: true, 
+    // optionsSuccessStatus: 200
+
+// }
+
+
 // middleware
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(
+    session({
+        store: new MongoStore({url: process.env.MONGODB_URI}),
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+        }
+    })
+)
 
 app.use('/api/v1', routes.auth);
 app.use('/api/v1', routes.api);
