3a79c2344d61376ca07ab9b391a9b49adc7871c0
leighannbaxa <leighannbaxa@gmail.com>
2020-04-06 18:35:05 -0500

    adds city model, controller, routes, and cityPostsIndex controller

---
+++ wayfarer-backend/models/City.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CitySchema = new Schema({
+    name: String,
+    description: String,
+
+})
+
+const City = mongoose.model('City', CitySchema);
+
+module.exports = City;
--- wayfarer-backend/routes/api.js
+++ wayfarer-backend/routes/api.js
@@ -10,10 +10,18 @@ router.put('/users/:id', ctrl.users.update);
 router.delete('/users/:id', ctrl.users.destroy);
 
 router.get('/posts', ctrl.posts.index);
-router.get('/users/:id/posts', ctrl.posts.userPostIndex);
 router.get('/posts/:id', ctrl.posts.show);
 router.post('/posts', ctrl.posts.create);
 router.put('/posts/:id', ctrl.posts.update);
 router.delete('/posts/:id', ctrl.posts.destroy);
+
+router.get('/cities', ctrl.cities.index);
+router.get('/cities/:id', ctrl.cities.show);
+router.post('/cities', ctrl.cities.create);
+router.put('/cities/:id', ctrl.cities.update);
+router.delete('/cities/:id', ctrl.cities.destroy);
+
+router.get('/users/:id/posts', ctrl.posts.userPostIndex);
+router.get('/cities/:id/posts', ctrl.posts.cityPostIndex);
 
 module.exports = router;
