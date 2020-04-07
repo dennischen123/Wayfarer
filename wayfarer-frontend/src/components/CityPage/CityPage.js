c486dc51b94b7b7ee349713f85e95f9ee90622aa
dennischen123 <dennischen123@gmail.com>
2020-04-06 20:33:15 -0700

    added cities page with static data

---
+++ wayfarer-frontend/src/components/CityPage/CityPage.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import PostContainer from '../../containers/PostContainer';
+import CityContainer from '../../containers/CityContainer';
+import currentUser from '../../api/currentUser';
+import CityHeading from '../CityHeading/CityHeading';
+
+export default class CityPage extends React.Component {
+    state = {
+        cityClicked: "3A123xdf2341",
+        cityClickedPosts: [{
+            _id: "2A123sx2341",
+            title: "title",
+            content: "content"
+        },
+            {
+                _id: "2A123sx2341",
+                title: "title",
+                content: "content"
+            },],
+        cities : [
+            {
+                _id: "3A123xdf2341",
+                name: "San Francisco",
+                country: "United States",
+                image: "",
+            },
+            {
+                name: "London",
+                country: "United States",
+                image: "",
+            },
+            {
+                _id: "4A123sxdf341",
+
+                name: "Chicago",
+                country: "United States",
+                image: "",
+            },
+            {
+                _id: "1A13sxdf2341",
+
+                name: "Sydney",
+                country: "Australia",
+                image: "",
+            }],
+        posts : [
+            {
+                _id: "2A123sx2341",
+                title:"title",
+                content:"content"
+            },
+            {
+                _id: "2A123sx2341",
+                title:"title",
+                content:"content"
+            },
+            {
+                _id: "3A123sx341",
+                title:"title2",
+                content:"content2"
+            }]
+    }
+
+    componentDidMount(){
+        //get cities from db
+    }
+
+    render() {
+        return (
+            <div className="CityPage container">
+                <h1>City Page</h1>
+                <div className="row">
+                    <div className="col-5 bg-dark">
+                        <CityContainer cities={this.state.cities}/>
+
+                    </div>
+                    <div className="col-7 bg-secondary">
+                        <div className="container d-flex h-100 flex-column">
+                            <div className="row h-25">
+                                <CityHeading />
+                            </div>
+                            <div className="row h-10">
+                                <button className="btn btn-primary">add</button>
+                            </div>
+                            <div className="row h-50">
+                                <PostContainer posts={this.state.posts} />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- wayfarer-frontend/src/containers/PostContainer.js
+++ wayfarer-frontend/src/containers/PostContainer.js
@@ -5,23 +5,29 @@ import axios from 'axios';
 
 export default class PostContainer extends React.Component {
     
-    componentDidMount() {
-        axios.get(`http://localhost:4000/api/v1/users/${currentUser.getUserId()}/posts`)
-            .then((res) => {
-                currentUser.setPosts(res.data);
-                this.forceUpdate();
-            })
-            .catch(err => console.log(err))
-    }
+    // componentDidMount() {
+    //     axios.get(`http://localhost:4000/api/v1/users/${currentUser.getUserId()}/posts`)
+    //         .then((res) => {
+    //             currentUser.setPosts(res.data);
+    //             this.forceUpdate();
+    //         })
+    //         .catch(err => console.log(err))
+    // }
 
     render() {
-        let posts = currentUser.getPosts();
-        if (posts)
-            posts = posts.map((post, index) => { return <Post post={post} key={post._id} /> })
+        // let posts = currentUser.getPosts();
+        // if (posts)
+            // posts = posts.map((post) => { return <Post post={post} key={post._id} /> })
+        let posts = this.props.posts.map((post) => { return <Post post={post} key={post._id} /> })
         return (
             <div className="PostContainer container">
-                <h4>post container</h4>
-                {posts}
+                {(posts.length > 1)
+                ? <>
+                    <h4>post container</h4> 
+                    {posts}
+                    </>
+                : <></>
+                }
             </div>
         );
     }
