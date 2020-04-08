5bc2f187e48371b7e50396b495a27070bb5d6787
dennischen123 <dennischen123@gmail.com>
2020-04-07 20:19:48 -0700

    merge

--- wayfarer-frontend/src/api/currentUser.js
+++ wayfarer-frontend/src/api/currentUser.js
@@ -1,4 +1,26 @@
+const currentUser = (function () {
+    let user = {};
+    let posts = [];
 
+    const getUser = () => {
+        return user;
+    };
+
+    const getUserId = () => {
+        return user._id;
+    };
+
+    const setUser = (data) => {
+        user = data;
+    };
+
+    const getPosts = () => {
+        return posts;
+    }
+
+    const setPosts = (data) => {
+        posts = data;
+    }
 
 
     return {
