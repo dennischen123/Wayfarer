cec660a1fd0a8b2ecdfb5248e17aee617b259a40
beniciovargas <beniciorvargas@gmail.com>
2020-04-07 20:09:42 -0700

    removes node modules

--- wayfarer-frontend/src/containers/CityContainer.js
+++ wayfarer-frontend/src/containers/CityContainer.js
@@ -7,15 +7,14 @@ export default class CityContainer extends React.Component {
 
     render() {
          axios.get (`http://localhost:4000/api/v1/cities`)
-        .then((res) => {
-            // let cities = (res.data)
-            // cities = this.state.cities
-            // let cities = this.state.cities.map((city) => { return <City city={city} key={city._id} />})
+        .then((res) => { 
+            let cities = (res.data)
+            cities.map((city) => { return <City city={city} key={city._id} />})
         })
         
         return (
             <div className="CityContainer overflow-auto">
-                {/* {cities} */}
+                {cities}
             </div>
         );
     }
