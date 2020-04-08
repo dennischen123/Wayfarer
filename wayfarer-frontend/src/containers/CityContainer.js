b1cc1e4ff69492ce829020ed9a6b83717eb8eafb
beniciovargas <beniciorvargas@gmail.com>
2020-04-07 19:20:56 -0700

    started logic in city container

--- wayfarer-frontend/src/containers/CityContainer.js
+++ wayfarer-frontend/src/containers/CityContainer.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import City from '../components/City/City';
 
 export default class CityContainer extends React.Component {
     render() {
          axios.get (`http://localhost:4000/api/v1/cities`)
-
         .then((res) => {
-            console.log(res);
-            // this.cities.map((city) => { return <City city={city} key={city._id} cityOnClick={this.props.cityOnClick}/>})
+            let cities = res.data;
         })
         
         return (
